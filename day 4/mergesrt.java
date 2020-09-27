3864266c96594905face9700f5ae547e5025f61a
PRATYUSH MAHAPATRA <18btcse42@suiit.ac.in>
2020-09-27 22:29:27 +0530

    Create mergesrt.java
    Learnt mergesort
    I am lazy today so only one sorting technique.

---
+++ day 4/mergesrt.java
@@ -0,0 +1,52 @@
+package sort;
+
+/**
+ * mergesrt
+ */
+public class mergesrt {
+
+    public static void main(final String[] args) {
+        final int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        mergeSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    // { 20, 35, -15, 7, 55, 1, -22 }
+    public static void mergeSort(final int[] input, final int start, final int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+
+        final int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    // { 20, 35, -15, 7, 55, 1, -22 }
+    public static void merge(final int[] input, final int start, final int mid, final int end) {
+
+        if (input[mid - 1] <= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        final int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+
+    }
+}
