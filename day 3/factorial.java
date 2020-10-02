7cde27524d0275b6f3e44427d65aeffc803fde67
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2020-10-02 08:46:16 +0530

    new section added

--- day 3/factorial.java
+++ day 3/factorial.java
@@ -1,35 +1,34 @@
-package sort;
 
 public class factorial {
     public static void main(final String[] args) {
 
-        System.out.println("Iterative factorial: "+fact(3));
+        System.out.println("Iterative factorial: " + fact(3));
 
-        System.out.println("recursive factorial: "+recursivefact(3));
+        System.out.println("recursive factorial: " + recursivefact(3));
 
     }
 
     public static int fact(final int num) {
 
-        if(num==0)
-        return 1;
-        int factorial =1;
+        if (num == 0)
+            return 1;
+        int factorial = 1;
         for (int i = 1; i <= num; i++) {
 
-            factorial*=i;
-            
+            factorial *= i;
+
         }
-        return factorial; 
-        
+        return factorial;
+
     }
 
     public static int recursivefact(final int num) {
 
-        if(num==0)
-        return 1;
+        if (num == 0)
+            return 1;
         // N!= N*(N-1)!
-        return num*recursivefact(num-1); 
-        
+        return num * recursivefact(num - 1);
+
     }
 
 }
--- jeevan/SexyPrimeNumber.java
+++ jeevan/SexyPrimeNumber.java
@@ -1,3 +1,17 @@
+// Sexy Prime
+// In mathematics, Sexy Primes are prime numbers that differ from each other by six. For example, the numbers 5 and 11 are both sexy primes, because they differ by 6. If p + 2 or p + 4 (where p is the lower prime) is also prime.
+
+// They can be grouped as:
+
+// Sexy prime pairs : It is of the form (p, p + 6), where p and p + 6 are prime numbers.
+// Eg. (11, 17) is a sexy prime pairs.
+// Sexy prime triplets : Triplets of primes (p, p + 6, p + 12) such that p + 18 is composite are called sexy prime triplets.
+// Eg. (7, 13, 19) is a Sexy prime triplets.
+// Sexy prime quadruplets : Sexy prime quadruplets (p, p + 6, p + 12, p + 18) can only begin with primes ending in a 1 in their decimal representation (except for the quadruplet with p = 5).
+// Eg. (41, 47, 53, 59) is a Sexy prime quadruplets.
+// Sexy prime quintuplets : In an arithmetic progression of five terms with common difference 6, one of the terms must be divisible by 5, because the two numbers are relatively prime. Thus, the only sexy prime quintuplet is (5, 11, 17, 23, 29); no longer sequence of sexy primes is possible.
+// Given a range of the form [L, R].The task is to print all the sexy prime pairs in the range.
+// Refer :- https://www.geeksforgeeks.org/sexy-prime/
 import java.util.Arrays;
 
 public class SexyPrimeNumber {
